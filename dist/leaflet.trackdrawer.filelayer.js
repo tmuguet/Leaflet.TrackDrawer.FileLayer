5186631ea4ce1d5456ba43cd37d3bacaad187471
Thomas Muguet <hi@tmuguet.me>
2019-12-23 17:42:11 +0100

    Updated dependencies

--- dist/leaflet.trackdrawer.filelayer.js
+++ dist/leaflet.trackdrawer.filelayer.js
@@ -105,7 +105,7 @@ function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArra
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
@@ -254,7 +254,7 @@ L.TrackDrawer.Track.include({
                                   return _context.stop();
                               }
                             }
-                          }, _loop, this);
+                          }, _loop);
                         });
                         j = 0;
 
@@ -276,7 +276,7 @@ L.TrackDrawer.Track.include({
                         return _context2.stop();
                     }
                   }
-                }, _callee, this);
+                }, _callee);
               })(), "t0", 10);
 
             case 10:
@@ -332,7 +332,7 @@ L.TrackDrawer.Track.include({
                   return _context4.stop();
               }
             }
-          }, _callee3, this);
+          }, _callee3);
         }));
 
         return function (_x2) {
@@ -384,7 +384,7 @@ L.TrackDrawer.Track.include({
                         return _context5.stop();
                     }
                   }
-                }, _callee4, this);
+                }, _callee4);
               }));
 
               return function (_x3) {
